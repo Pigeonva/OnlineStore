3cdfb4f9d28f580fbe0f41d1f053a2b2a2373e62
Pigeonva <i-realman@mail.ru>
2022-10-23 14:09:31 +0300

    feat: create UI

--- OnlineStore/ViewController.swift
+++ OnlineStore/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var cartButton: UIButton!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func menuTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func cartTapped(_ sender: UIButton) {
+    }
 }
 
